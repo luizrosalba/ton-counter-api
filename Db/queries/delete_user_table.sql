6a53ed111b3c2c0c11ab055a4de5343e15de46ac
luiz <luizrosalba@gmail.com>
2021-09-20 18:53:18 -0300

    feat(sql): Adding Sql queryies
    Adding SqlQueries used to create User and Counter Table

---
+++ Db/queries/delete_user_table.sql
@@ -0,0 +1,2 @@
+USE Ton;
+DROP TABLE Users;
---
+++ Db/queries/insert-user.sql
@@ -0,0 +1,14 @@
+USE Ton;
+
+INSERT INTO Users (
+    UserName, 
+    Password, 
+    Email, 
+    Cpf 
+)
+VALUES (
+    'Luiz 2',
+    'teste',
+    'luiz2@luiz.com',
+    '123.456.789-11'
+);
