d93cb6d15dd511c10849248cb8cea0b5f5e7b93e
luiz <luizrosalba@gmail.com>
2021-09-20 18:52:12 -0300

    fix(apiController): Fixing apiController name
    Fixing apiController name to comply to standart
    Removing unused Code

--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -6,29 +6,6 @@ class ApiController {
     ///////////////////////
     /*           Routes */
     ///////////////////////
-    getAll = async (req, res) => {
-        let example = await ApiModel.find({},"table_example");
-        if (!example.length) {
-            return Promise.reject('example not found').catch(err => {
-                throw new HttpException(404, 'example not found');
-            });
-        }
-        res.send(example);
-    };
-    
-    getOne = async (req, res) => {
-        const {
-            id
-        } = req.body /// x-www-form-urlencoded 
-        let output = ''
-        if (id)
-            output = await ApiModel.findOne({ 
-                actor_id: id},
-                "actor"
-            );
-        return(output);
-    };
-
     accesses = async (req, res) => {
         const {
             key, 
